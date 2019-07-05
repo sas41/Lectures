2fa252fc89b80112bd4f7ef36091413d71a0b4c3
Berk A <berkonvo@gmail.com>
2019-07-05 14:01:52 +0300

    Added Recursive Permutations and Combinations

---
+++ Module 4 - Algorithms and Data Structures/Algorithms/RecursiveCombinations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveCombinations
+{
+    class Program
+    {
+        public static void PrintCombinations(int k, int n, List<int> inputList)
+        {
+            List<int> list = new List<int>();
+            list.AddRange(inputList);
+
+            if (list.Count < k)
+            {
+                if (list.Count == 0)
+                {
+                    list.Add(0);
+                }
+                else
+                {
+                    list.Add(list[list.Count - 1] - 1);
+                }
+
+                for (int i = list[list.Count - 1]; i < n; i++)
+                {
+                    list[list.Count - 1]++;
+                    PrintCombinations(k, n, list);
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", list));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrintCombinations(3, 3, new List<int>());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Module 4 - Algorithms and Data Structures/Algorithms/RecursivePermutations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursivePermutations
+{
+
+    class Program
+    {
+        public static void PrintPermutations(int n, List<int> inputList)
+        {
+            List<int> list = new List<int>();
+            list.AddRange(inputList);
+
+            if (list.Count < n)
+            {
+                list.Add(0);
+                for (int i = 1; i <= n; i++)
+                {
+                    list[list.Count - 1]++;
+                    PrintPermutations(n, list);
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", list));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrintPermutations(3, new List<int>());
+            Console.ReadLine();
+        }
+    }
+}
