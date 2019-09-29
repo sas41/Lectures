b336e67c4c36f333f22343545a367a691ed320fe
Berk A <sas41@users.noreply.github.com>
2019-09-29 13:23:16 +0300

    Added Examples for Streams and Lambdas

---
+++ Module 5 - OOP Part 2/OOP Part 2/FunctionalProgramming/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReadAndFilterNumbers();
+            SortShortWords();
+            Console.ReadLine();
+        }
+
+        static void ReadAndFilterNumbers()
+        {
+            var collection = Console.ReadLine()
+                                    .Split()
+                                    .Select(x => int.Parse(x))
+                                    .Where(y => y % 2 == 0)
+                                    .OrderBy(y => y);
+
+            Console.WriteLine(string.Join(" ", collection));
+        }
+
+        static void SortShortWords()
+        {
+            char[] separators = new char[] {'.',',',':',';','(',')','[',']','\\','\"','\'','/','!','?',' '};
+            string input = Console.ReadLine().ToLower();
+            string[] words = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            var resultCollection = words
+                                    .Where(word => word.Length < 5)
+                                    .Distinct()
+                                    .OrderBy(word => word);
+
+            Console.WriteLine(string.Join(", ", resultCollection));
+        }
+    }
+}
---
+++ Module 5 - OOP Part 2/OOP Part 2/Streams/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"E:\Projects\C#\Lectures\Module 5 - OOP Part 2\OOP Part 2\Streams\test.txt";
+            StringBuilder fileLines = new StringBuilder();
+
+            using (StreamReader reader = new StreamReader(filepath))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    fileLines.Append(line);
+                    fileLines.Append(Environment.NewLine);
+                    Console.WriteLine(line);
+                    line = reader.ReadLine();
+                }
+            }
+
+            string input = Console.ReadLine();
+            while (input != "")
+            {
+                fileLines.Append(input);
+                fileLines.Append(Environment.NewLine);
+                input = Console.ReadLine();
+            }
+
+            using (StreamWriter writer = new StreamWriter(filepath))
+            {
+                writer.Write(fileLines);
+
+                // Alternative
+                //List<string> lines = fileLines.ToString().Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                //foreach (var line in lines)
+                //{
+                //    writer.WriteLine(line);
+                //}
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
