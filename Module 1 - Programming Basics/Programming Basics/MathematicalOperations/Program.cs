83cdcf7fa15e72255472d4dcb378b2ba0134e04d
Berk A <sas41@users.noreply.github.com>
2019-11-09 13:36:39 +0200

    Module 1

---
+++ Module 1 - Programming Basics/Programming Basics/MathematicalOperations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MathematicalOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            input = Console.ReadLine();
+            int otherNumber = int.Parse(input);
+
+            int sum = number + otherNumber;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
