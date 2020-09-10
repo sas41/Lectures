4b43ba1d826fb5e0a1d42fcb5ed8e19ef7a1df64
Berk A <sas41@users.noreply.github.com>
2020-09-10 12:22:02 +0300

    Folder Restructure

---
+++ Module 10 - Algorithms and Data Structures/ADSExercises/FibonacciDP/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciDP
+{
+    class Program
+    {
+        static List<int> Fibs;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Fibs = (new int[n+1]).ToList();
+
+            Fibs[1] = 1;
+            Fibs[2] = 1;
+
+            FibGood(n);
+            Console.WriteLine(string.Join(" ", Fibs.Skip(1)));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            int bad = FibBad(n);
+            Console.WriteLine(bad);
+
+            Console.ReadLine();
+        }
+
+        static int FibGood(int n)
+        {
+            if (n<=2)
+            {
+                return 1;
+            }
+            else if (Fibs[n] != 0)
+            {
+                return Fibs[n];
+            }
+
+            int nm1 = FibGood(n - 1);
+            int nm2 = FibGood(n - 2);
+
+            Fibs[n] = nm1 + nm2;
+
+            return nm1 + nm2;
+        }
+
+
+        static int FibBad(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+
+            int nm1 = FibBad(n - 1);
+            int nm2 = FibBad(n - 2);
+
+            return nm1 + nm2;
+        }
+    }
+}
---
+++ Module 10 - Algorithms and Data Structures/ADSExercises/KnapsackProblem/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnapsackProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://www.youtube.com/watch?v=8LusJS5-AGo
+
+            int maxWeight = 7;
+            
+            int[] weights = new int[] { 1, 3, 4, 5 };
+            int[] values = new int[]  { 1, 4, 5, 7 };
+
+            if (weights.Length != values.Length)
+            {
+                Console.WriteLine("Amount of items mismatch between weights array and values array.");
+                return;
+            }
+
+            int itemCount = weights.Length;
+
+            int[,] knapsack = new int[maxWeight + 1, itemCount];
+
+
+            for (int item = 0; item < itemCount; item++)
+            {
+                for (int capacity = 1; capacity <= maxWeight; capacity++)
+                {
+                    if (weights[item] <= capacity)
+                    {
+                        int currentMax = values[item];
+
+                        int remainder = capacity - weights[item];
+                        if (item != 0)
+                        {
+                            currentMax += knapsack[remainder, item - 1];
+                        }
+
+                        if (item != 0 && currentMax < knapsack[capacity, item - 1])
+                        {
+                            knapsack[capacity, item] = knapsack[capacity, item - 1];
+                        }
+                        else
+                        {
+                            knapsack[capacity, item] = currentMax;
+                        }
+                    }
+                    else
+                    {
+                        knapsack[capacity, item] = knapsack[capacity, item - 1];
+                    }
+                }
+            }
+
+            // Print the table (works for single digit tables)
+
+            Console.Write("______|_");
+            for (int i = 0; i <= maxWeight; i++)
+            {
+                Console.Write($"_{i}_|_");
+            }
+            Console.WriteLine();
+            for (int j = 0; j < itemCount; j++)
+            {
+                Console.Write($"({values[j]}) {weights[j]} | ");
+                for (int i = 0; i <= maxWeight; i++)
+                {
+                    Console.Write($" {knapsack[i, j]} | ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
